556696cfd8e8a0e904975933a859d92e504c81c3
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-02 13:15:38 +0700

    feat: eksekusi sql

---
+++ sql_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestExecSql(t *testing.T) {
+	db := GetConnnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "INSERT INTO customer(id, name) VALUES(2, 'Firlana')"
+	_, err := db.ExecContext(ctx, script)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Success insert data new customer")
+}
