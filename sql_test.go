178c4cf1b109bf25411b130a23f4a763e08249f5
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-03 12:21:10 +0700

    feat: add sql injection

--- sql_test.go
+++ sql_test.go
@@ -94,3 +94,34 @@ func TestQuerySqlComplex(t *testing.T) {
 		fmt.Println("Created At : ", createdAt)
 	}
 }
+
+func TestSqlInjection(t *testing.T) {
+	db := GetConnnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin"
+	// username := "admin' #"
+	password := "admin"
+
+	script := "SELECT username FROM user WHERE username = '" + username + "' AND password = '" + password + "' LIMIT 1"
+	// fmt.Println(script)
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Login success : ", username)
+	} else {
+		fmt.Println("Login failed")
+	}
+}
